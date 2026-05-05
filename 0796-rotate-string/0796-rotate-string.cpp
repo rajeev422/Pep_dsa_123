0c9c469f47db7ea26678d28eeb9b3341eda2f8d8
Rajeev_Choudhary <150056490+rajeev422@users.noreply.github.com>
2026-05-05 16:18:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.size();
+        if(s.size()!=goal.size())return false;
+        string neww=s+s;
+        int j=0;
+        for(int i=0;i<2*n;i++){
+            if
+        }
+    }
+};
